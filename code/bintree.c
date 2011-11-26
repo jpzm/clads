340c2ed2292cd75d18a5d0f691850f4f389150f8
Joao Paulo S. Medeiros <jpsm1985@gmail.com>
2011-11-26 02:04:27 -0300

    Add binary tree package.

---
+++ code/bintree.c
@@ -0,0 +1,79 @@
+/**
+ * Copyright (C) 2011 Joao Paulo de Souza Medeiros
+ *
+ * Author(s): Joao Paulo de Souza Medeiros <ignotus21@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#include "bintree.h"
+
+
+void
+clads_bintree_initialize(clads_bintree_type *t)
+{
+    // TODO
+}
+
+void
+clads_bintree_finalize(clads_bintree_type *t)
+{
+    // TODO
+}
+
+int
+clads_bintree_copy(const clads_bintree_type *ta,
+                   clads_bintree_type *tb)
+{
+    // TODO
+    return 0;
+}
+
+void
+clads_bintree_node_initialize(clads_bintree_node_type *n)
+{
+    // TODO
+}
+
+void
+clads_bintree_node_finalize(clads_bintree_node_type *n)
+{
+    // TODO
+}
+
+clads_bintree_node_type *
+clads_bintree_add_node(clads_bintree_type *t,
+                       clads_bintree_node_type *n)
+{
+    // TODO
+    return NULL;
+}
+
+clads_bintree_node_type *
+clads_bintree_search_node_from(clads_bintree_type *t,
+                               clads_bintree_node_type *r,
+                               clads_bintree_node_type *n)
+{
+    // TODO
+    return NULL;
+}
+
+clads_bintree_node_type *
+clads_bintree_search_node(clads_bintree_type *t,
+                          clads_bintree_node_type *n)
+{
+    // TODO
+    return NULL;
+}
